862f0d14c1338903748557a562a05ef780fffde2
Pablo Contreras Rosado <pconros2109@g.educaand.es>
2024-04-12 10:35:15 +0200

    Se añade un programa para obtener la localización de una lista de direcciones IPs

---
+++ IP_geolocalizacion.py
@@ -0,0 +1,17 @@
+
+import os, requests
+
+listado_final = []
+url = "https://reallyfreegeoip.org/json/"
+
+with open(r"C:\Users\Pablo\Downloads\IPs_unicas.txt", "r") as listado:
+    lineas = listado.readlines()
+    for linea in lineas:
+        ip = linea.strip()
+        respuesta = requests.get(url + ip)
+        respuesta.raise_for_status()
+        listado_final.append(respuesta.json())
+
+with open(r"C:\Users\Pablo\Downloads\IPs_y_geolocalizacion.txt", "w", encoding="utf-8") as archivo_final:
+    for datos_ip in listado_final:
+        archivo_final.write(str(datos_ip) + '\n')
