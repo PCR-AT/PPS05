0a70aae2964f362be443a72640b0babce18a44b4
Pablo Contreras Rosado <pconros2109@g.educaand.es>
2024-04-12 13:26:27 +0200

    Añadido comentario sobre el bloque de escritura.

--- IP_geolocalizacion.py
+++ IP_geolocalizacion.py
@@ -13,6 +13,7 @@ with open(r"C:\Users\Pablo\Downloads\IPs_unicas.txt", "r") as listado:
         respuesta.raise_for_status()
         listado_final.append(respuesta.json())
 
+# Escritura del archivo resultante.
 with open(r"C:\Users\Pablo\Downloads\IPs_y_geolocalizacion.txt", "w", encoding="utf-8") as archivo_final:
     for datos_ip in listado_final:
         archivo_final.write(str(datos_ip) + '\n')
